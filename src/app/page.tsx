e6423e0c8fc605b6a0b3190ff0f37851019caaf9
Eugene Kim <eugenedwk@gmail.com>
2024-12-30 13:16:20 -0500

    add mailbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import HeroBanner from "~/components/HeroBanner";
 import NavBar from "~/components/NavBar";
 import { BrowserWarning } from "~/components/BrowserWarning";
 import { isInAppBrowser } from "~/lib/browserDetection";
+import { Mailbox } from "lucide-react";
 
 export default async function HomePage() {
   const session = await getServerSession(authOptions);
@@ -22,19 +23,24 @@ export default async function HomePage() {
         <BrowserWarning isOpen={isUnsafeBrowser} />
       </div>
       <div className="hero-banner w-full bg-green-800 px-4 py-12 text-center">
-        <h1 className="text-xs font-extrabold tracking-tight text-white">
-          <div className="group relative inline-block w-full">
-            Dedicated to Sam Youn.
-            <div className="absolute left-1/2 top-full hidden -translate-y-16 translate-x-1/2 transform group-hover:block">
-              <img
-                src="/samtheman.jpg"
-                alt="He's the man"
-                className="h-32 w-32 object-cover"
-              />
-            </div>
+        <div className="group relative inline-flex items-center gap-2">
+          <Mailbox className="h-4 w-4 cursor-pointer text-white" />
+          <div className="absolute bottom-full left-1/2 mb-2 hidden -translate-x-1/2 transform rounded-md bg-green-900 p-4 shadow-lg group-hover:block">
+            <h1 className="text-xs font-extrabold tracking-tight text-white">
+              <div className="relative inline-block">
+                Dedicated to Sam Youn.
+                <div className="absolute left-1/2 top-full hidden -translate-y-16 translate-x-1/2 transform group-hover:block">
+                  <img
+                    src="/samtheman.jpg"
+                    alt="He's the man"
+                    className="h-32 w-32 object-cover"
+                  />
+                </div>
+              </div>
+              <div>He&apos;s single and ready to mingle.</div>
+            </h1>
           </div>
-          He&apos;s single and ready to mingle.
-        </h1>
+        </div>
       </div>
     </main>
   );
